4ed6fcd6a5133ac7623e64a2cf882c211d5959ca
prahveent <prahveent@gmail.com>
2018-09-17 18:50:04 +0530

    add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,19 @@
 class CommentsController < ApplicationController
 
   def create
-    
+    @forum = Forum.find(params[:forum_id])
+    @comment = @forum.comments.create(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      redirect_to forum_path(@forum)
+    else
+      render 'new'
+    end
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
