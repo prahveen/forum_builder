566c2737d07be66f9783a8ac83e75036565ead92
prahveent <prahveent@gmail.com>
2018-09-17 19:15:19 +0530

    comments for post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
+  before_action :find_forum
+  before_action :find_comment, only: [:edit, :update, :destroy]
 
   def create
-    @forum = Forum.find(params[:forum_id])
     @comment = @forum.comments.create(comment_params)
     @comment.user_id = current_user.id
 
@@ -12,8 +13,27 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to forum_path(@forum)
+    else
+      render 'edit'
+    end
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:content)
+    end
+
+    def find_forum
+      @forum = Forum.find(params[:forum_id])
+    end
+
+    def find_comment
+      @comment = @forum.comments.find(params[:id])
     end
 end
